899e8c233ed5d055aa5016c3db2a0fb739798194
khalilNayakawadi <136471783+khalilNayakawadi@users.noreply.github.com>
2024-09-05 02:39:12 +0530

    Update game.js

--- game.js
+++ game.js
@@ -14,7 +14,8 @@ for (const box of boxes) {
             turnO = true;
         }
         box.disabled = true;
-    })
+        checkWinner();
+    } )
 }
 
 resetbtn.addEventListener("click",()=>{
@@ -23,3 +24,40 @@ resetbtn.addEventListener("click",()=>{
         box.disabled = false;
     }
 })
+
+
+let winningPatterns = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [2,4,6],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8]
+]
+
+const checkWinner = ()=>{
+    for (const pattern of winningPatterns) {
+
+        let posVal1 = boxes[pattern[0]].innerText;
+        let posVal2 = boxes[pattern[1]].innerText;
+        let posVal3 = boxes[pattern[2]].innerText;
+        if(posVal1 != "" && posVal2 != "" && posVal3 != ""){
+            if(posVal1 == posVal2 && posVal2 == posVal3)
+            {
+                if(posVal1 === "O")
+                {
+                    document.querySelector(".heading").innerText = "Player 1 Win 👑"
+                }
+                else{
+                     document.querySelector(".heading").innerText = "Player 2 Win👑"
+                }
+
+            }
+        }
+    }
+   
+    
+}
+
